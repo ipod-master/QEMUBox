37f8710f8d376c387ebf58a4ffc6ae155ad2fd0f
DVT Digest <tomassegoocococo@gmail.com>
2026-08-27 04:37:28 -0700

    Create Package.swift for Swift Package Manager configuration

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "QEMUBox",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "QEMUBox",
+            targets: ["QEMUBox"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QEMUBox",
+            dependencies: [],
+            path: "QEMUBoxApp"
+        )
+    ]
+)
